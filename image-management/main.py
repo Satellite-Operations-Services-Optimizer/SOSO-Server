951a8e876a2d22fdb855026f2366683cb850e9ce
Stanley Ihesiulo <stanihe1901@gmail.com>
2023-10-15 21:33:59 -0400

    started working on image request api endpoint

--- image-management/main.py
+++ image-management/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
 
-@app.get("/home")
+class ImageRequest(BaseModel):
+    latitude: float
+    longitude: float
+    priority: Literal["low", "medium", "high"] = "low"
+    resolution: Literal["low", "medium", "high"]
+    imaging_time: TimeRange = Depends()
+    deliver_by: datetime
+    revisit: Repeat
+
+
+
+@app.get("/")
 def index():
+    return {"message": "Hello, I am the image management server!"}
+
+@app.get("/request_image")
+def index(region: ImageRequest = Depends()):
     return {"message": "Hello, I am the image management server!"}
