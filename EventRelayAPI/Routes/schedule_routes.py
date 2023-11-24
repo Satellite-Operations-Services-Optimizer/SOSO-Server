4c125ffdec6b1d455a3fc3e3c2ef278a247df946
Hashir Jamil <hashir.jamil17@gmail.com>
2023-11-24 00:43:20 -0500

    [ADD][SOSO-103] return all schedules, by id, join

--- EventRelayAPI/Routes/schedule_routes.py
+++ EventRelayAPI/Routes/schedule_routes.py
@@ -1,10 +1,20 @@
+from typing import List
 from fastapi import APIRouter, Body, Depends
 import logging
-from Helpers.schedule_utils import get_all_schedules
+from Models.ScheduleModel import ScheduleModel
+from Helpers.schedule_utils import get_all_basic_schedules, get_all_joined_schedules, get_basic_schedule_by_id
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
-@router.get("/")
-async def get_schedules(): 
-    return get_all_schedules()
+@router.get("/", response_model=List[ScheduleModel])
+async def get_basic_schedules(): 
+    return get_all_basic_schedules()
+
+@router.get("/id{id}")
+async def get_schedule_by_id(id):
+    return get_basic_schedule_by_id(id)
+
+@router.get("/complete")
+async def get_full_schedule():
+    return get_all_joined_schedules()
