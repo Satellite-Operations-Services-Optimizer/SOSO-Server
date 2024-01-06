88f880cced6d060ef85a389cbda939776169f362
Walid AlDari <walidald@my.yorku.ca>
2024-01-06 17:16:46 -0500

    Updated the Request Models

--- ImageManagementService/Models/RequestModel.py
+++ ImageManagementService/Models/RequestModel.py
@@ -6,6 +6,12 @@ class RequestDetails(BaseModel):
     requestTime: Optional[datetime] = None
     requestType: str
     
+class Recurrence(BaseModel):
+    Revisit: str
+    NumberOfRevisits: Optional[int] = None
+    RevisitFrequency: Optional[int] = None
+    RevisitFrequencyUnits: Optional[str] = None
+
 class ImageRequest(BaseModel):
     Latitude: float
     Longitude: float
@@ -14,7 +20,7 @@ class ImageRequest(BaseModel):
     ImageStartTime: str
     ImageEndTime: str
     DeliveryTime: str
-    RevisitTime: str
+    Recurrence: Recurrence
     
 class ImageOrder(BaseModel):
     id: int
@@ -30,3 +36,4 @@ class ImageOrder(BaseModel):
     retake_count: int
     retake_freq_min: int
     retake_freq_max:int
+    
