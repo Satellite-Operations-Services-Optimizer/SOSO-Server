806f1ca13c991c5153e42d00abb3c4e50288b5af
Hashir Jamil <hashir.jamil17@gmail.com>
2024-02-08 16:10:00 -0500

    [DELETE] login/registration service

--- login_registration_service/models/LRSConsumerEventData.py
+++
@@ -1,8 +0,0 @@
-from Models.QueueModel import QueueRequest, QueueDetails
-from pydantic import BaseModel
-from typing import Optional
-
-
-class LRSConsumerEvenData(BaseModel):
-    message: QueueRequest = None
-    details: Optional[QueueDetails] = None
