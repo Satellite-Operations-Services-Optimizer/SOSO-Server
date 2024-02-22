993303ee26aad3ce8de710a1170670db7546ea5f
Walid AlDari <walidald@my.yorku.ca>
2024-02-22 12:57:31 -0500

    Finalized Eclipse Opportunity finding tests

--- scheduler_service/tests/integrationTest.py
+++ scheduler_service/tests/integrationTest.py
@@ -10,17 +10,21 @@ from fastapi.encoders import jsonable_encoder
 from app_config.database.setup import get_session, Base
 from app_config.database.mapping import EclipseProcessingBlock, SatelliteEclipse
 
-# def get_satellite_eclipse(start_time: datetime, end_time:datetime):
-#     session = get_session();
-#     results = session.query()
+async def get_satellite_eclipse(start_time: datetime, end_time:datetime):
+    session = get_session()
+    results = session.query(SatelliteEclipse).filter(SatelliteEclipse.start_time < end_time, SatelliteEclipse.start_time >= start_time).all()
+    session.close()
+    return results
 
 
 def clear_eclipse_db(start_time: datetime, end_time: datetime):
     session = get_session();
     try:
-        session.query(EclipseProcessingBlock).filter(datetime.strptime(EclipseProcessingBlock.time_range[0]) >= start_time, 
-        datetime.strptime(EclipseProcessingBlock.time_range[1]) <= end_time).delete()
+        session.query(SatelliteEclipse).filter(SatelliteEclipse.start_time < end_time, SatelliteEclipse.start_time >= start_time).delete()
+        
         session.commit()
+        
+        session.close()
     except Exception as e:
         print(f"An error occurred clearing rows: {e}")
         session.rollback()
@@ -51,38 +55,13 @@ def read_eclipse_file():
                         eclipseOutput[i][startdate] = enddate
                     except Exception:
                         print(f"Failed at line {line_count}")  
-                    # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
                     line_count += 1
     return eclipseOutput
 
-class SchedulerTests(unittest.IsolatedAsyncioTestCase):
+class SchedulerTests(unittest.IsolatedAsyncioTestCase):    
     
-    # #Test with basic functionality
-    # async def test_01_ensure_eclipse_events_populated(self):
-    #     start_timeJSON = {
-    #         "year":2024,
-    #         "month":2,
-    #         "day":15,
-    #         "hour":12
-    #     }
-        
-    #     end_timeJSON = {
-    #         "year":2024,
-    #         "month":2,
-    #         "day":16,
-    #         "hour":12
-    #     }
-        
-    #     start_time = datetime(**start_timeJSON)
-    #     end_time = datetime(**end_timeJSON)
-    #     ensure_eclipse_events_populated(start_time, end_time);
-        
-        
-        
-    #     self.assertEquals("","");
-    
-    
-    async def test_02_ensure_eclipse_events_populated(self):
+    #Test with valid input. Input being a 12 hour period. 
+    async def test_01_ensure_eclipse_events_populated(self):
         
         start_timeJSON = {
             "year":2024,
@@ -90,7 +69,6 @@ class SchedulerTests(unittest.IsolatedAsyncioTestCase):
             "day":15,
             "hour":12
         }
-        
         end_timeJSON = {
             "year":2024,
             "month":2,
@@ -101,11 +79,75 @@ class SchedulerTests(unittest.IsolatedAsyncioTestCase):
         start_time = datetime(**start_timeJSON)
         end_time = datetime(**end_timeJSON)
         
-        # ensure_eclipse_events_populated(start_time, end_time);
+        ensure_eclipse_events_populated(start_time, end_time);
+        
+        expected = read_eclipse_file();
+        actual = await get_satellite_eclipse(start_time, end_time);
         clear_eclipse_db(start_time, end_time)
         
-        self.assertEquals("","");
-    
+        for i in range(len(actual)):
+            currStart = actual[i].start_time.replace(tzinfo=None)
+            if currStart:
+                currSat = actual[i].asset_id
 
+                cond = False
+                for actualStart in expected[currSat].keys():
+                    diff = abs(actualStart-currStart)
+                    if diff.total_seconds() <= 350:
+                        print(f"{currStart} and {actualStart} passed")
+                        cond = True
+                        break
+                self.assertTrue(cond, msg=f"{currStart} has no match.");
+
+    #Test with invalid input. Start time occurring after the end time. 
+    async def test_02_ensure_eclipse_events_populated(self):
+        
+        start_timeJSON = {
+            "year":2024,
+            "month":2,
+            "day":15,
+            "hour":12
+        }
+        end_timeJSON = {
+            "year":2024,
+            "month":2,
+            "day":16,
+            "hour":12
+        }
+        
+        start_time = datetime(**start_timeJSON)
+        end_time = datetime(**end_timeJSON)
+        
+        ensure_eclipse_events_populated(end_time, start_time);
+        
+        actual = await get_satellite_eclipse(end_time, start_time);
+        
+        self.assertTrue(len(actual) == 0)
+
+
+    #Test with invalid input. Start time and end time being the exact same. 
+    async def test_03_ensure_eclipse_events_populated(self):
+        start_timeJSON = {
+            "year":2024,
+            "month":2,
+            "day":15,
+            "hour":12
+        }
+        end_timeJSON = {
+            "year":2024,
+            "month":2,
+            "day":15,
+            "hour":12
+        }
+        
+        start_time = datetime(**start_timeJSON)
+        end_time = datetime(**end_timeJSON)
+        
+        ensure_eclipse_events_populated(start_time, end_time);
+        
+        actual = await get_satellite_eclipse(start_time, end_time);
+        
+        self.assertTrue(len(actual) == 0)
+        
 if __name__ == '__main__':
     unittest.main()
