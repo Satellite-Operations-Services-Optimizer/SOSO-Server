1041c2044991ae9af792c840c38cb32c859033e9
Ruth Bezabeh <ruttkas@my.yorku.ca>
2024-03-06 15:59:14 -0500

    Update outbound Integration test

--- ground_station_out_bound_service/Tests/Integrationtest.py
+++ ground_station_out_bound_service/Tests/Integrationtest.py
@@ -1,9 +1,15 @@
-from app_config.database.setup import scoped_session as session
-from app_config.database.mapping import ContactEvent, ScheduledEvent, ImageOrder, OutboundSchedule
-from datetime import datetime
+from app_config import get_db_session
+from app_config.database.mapping import ContactEvent, ScheduledEvent, ImageOrder, OutboundSchedule, Satellite, GroundStation, Schedule, ScheduledImaging, ScheduleRequest
+from datetime import datetime, timedelta
 from fastapi.encoders import jsonable_encoder
-def get_scheduled_contacts():
-    contacts = session.query(ContactEvent).filter(ContactEvent.window_start >= datetime.now(), ContactEvent.window_start <= datetime.minute(29) ).all()
+from ground_station_out_bound_service.Tests.populate_scheduled_events import create_valid_image_order_schedule
+from sqlalchemy import or_
+import random
+
+session = get_db_session()
+
+def get_approaching_scheduled_contacts():
+    contacts = session.query(ContactEvent).filter(ContactEvent.start_time >= datetime.now()).filter(ContactEvent.start_time <= (datetime.now() + timedelta(minutes=29)) ).all()
     return contacts
 
 def get_image_orders():
@@ -11,7 +17,7 @@ def get_image_orders():
     return orders
 
 def check_outbound_schedule(contactId: int):
-    outbound_schedule = session.query(OutboundSchedule).filter(OutboundSchedule.contactId == contactId).first()
+    outbound_schedule = session.query(OutboundSchedule).filter(OutboundSchedule.contact_id == contactId).first()
     if (outbound_schedule != None):
         if(outbound_schedule.status == "sent_to_gs"):
             return "schedule_sent"
@@ -20,7 +26,7 @@ def check_outbound_schedule(contactId: int):
     return "schedule_not_created";
 
 def check_schedule_status():
-    contacts = get_scheduled_contacts()
+    contacts = get_approaching_scheduled_contacts()
     schedule_sent = []        # success scenario
     schedule_not_sent = []    # should be an error
     schedule_not_created = [] # should give warning
@@ -38,17 +44,69 @@ def check_schedule_status():
     print("\nthe outbound_schedule was marked as not sent for the following contacts: \n", schedule_not_sent)
     
     return schedule_not_sent 
+def cleanup():
+    schedules = session.query(Schedule).filter(Schedule.group_name == "test_gsoutbound").all()
+    print(jsonable_encoder(schedules))
+    for schedule in schedules:
+        imageorders = set()
+        contacts = session.query(ContactEvent).filter(ContactEvent.schedule_id == schedule.id).all()
+        print(jsonable_encoder(contacts))
+        for contact in contacts:
+            scheduledimages = session.query(ScheduledImaging).filter(or_(ScheduledImaging.uplink_contact_id== contact.id, ScheduledImaging.downlink_contact_id== contact.id ) ).all()
+            print(scheduledimages)
+            for scheduledimage in scheduledimages:
+                print("\n1111\n")
+                print(scheduledimage)
+                schedulerequest = session.query(ScheduleRequest).filter(ScheduleRequest.id == scheduledimage.request_id).first()
+                imageorder = session.query(ImageOrder).filter(ImageOrder.id == schedulerequest.order_id).first()
+                imageorders.add(imageorder)
+                
+                
+                session.delete(scheduledimage)
+                session.delete(schedulerequest)
+                
+                
+            outbound_schedule = session.query(OutboundSchedule.contact_id == contact.id).first()            
+            if(outbound_schedule != None):
+                session.delete(outbound_schedule)
+                #outbound_schedule.delete(synchronize_session='fetch')
+            
+            session.delete(contact)
+            #contact.delete(synchronize_session='fetch')
+       
+        for imageorder in imageorders:
+            session.delete(imageorder)
+        session.delete(schedule)
+        # schedule.delete(synchronize_session='fetch')
+        session.commit()
 
-contacts_with_schedules_not_sent  = check_schedule_status()
-if(contacts_with_schedules_not_sent != None):
-    print("\nThese outbound schedules were not sent: \n")
-    for contact in contacts_with_schedules_not_sent:
-        schedule = session.query(OutboundSchedule).filter(OutboundSchedule.contactId == contact.id).first()
-        print(jsonable_encoder(schedule), "\n")  
+    
+            
+    
+def initialize():
+    cleanup()    
+    for i in range(1,10):
+      create_valid_image_order_schedule(datetime.now() + timedelta(minutes=i*3), f"testschedule {i}")
+    for i in range(10,15):
+      create_valid_image_order_schedule(datetime.now() + timedelta(minutes=i*3), f"testschedule {i}")
 
 
-orders = jsonable_encoder(get_image_orders())
-print(orders)
-                  
-#contacts = jsonable_encoder(get_scheduled_contacts())
-#print(contacts)
+def run_test():
+    initialize()
+
+    contacts_with_schedules_not_sent  = check_schedule_status()
+    if(contacts_with_schedules_not_sent != None):
+        print("\nThese outbound schedules were created but not sent: \n")
+        for contact in contacts_with_schedules_not_sent:
+            schedule = session.query(OutboundSchedule).filter(OutboundSchedule.contact_id == contact.id).first()
+            print(jsonable_encoder(schedule), "\n")  
+
+                    
+    # contacts = jsonable_encoder(get_approaching_scheduled_contacts())
+    # print(contacts)
+
+    # contacts = session.query(ContactEvent).all()
+    # print(jsonable_encoder(contacts))
+
+if __name__ == "__main__":
+    run_test()
---
+++ ground_station_out_bound_service/Tests/populate_scheduled_events.py
@@ -0,0 +1,167 @@
+from app_config import get_db_session, logging
+from app_config.database.mapping import Satellite, GroundStation, Schedule, ContactEvent, ScheduledImaging, ScheduledMaintenance, ScheduleRequest, ImageOrder
+from datetime import datetime, timedelta
+from math import ceil
+import random
+
+logger = logging.getLogger(__name__)
+
+def populate_scheduled_events():
+    logger.info("Populating various schedules...")
+    create_valid_image_order_schedule(datetime.now())
+
+def create_valid_image_order_schedule(start_time: datetime, schedule_name: str):
+    random.seed(123)
+    session = get_db_session()
+    schedule = Schedule(name=schedule_name, group_name="test_gsoutbound")
+    session.add(schedule)
+
+    satellites = session.query(Satellite).limit(3).all()
+    ground_stations = session.query(GroundStation).limit(2).all()
+    logger.info(f"Populating schedule '{schedule_name}' with a scheduled image order using random satellites and groundstations...")
+
+
+    if not len(satellites):
+        raise Exception("No satellites in database. Need at least one satellite to create a schedule.")
+    elif not len(ground_stations):
+        raise Exception("No ground stations in database. Need at least one ground station to create a schedule.")
+
+
+    image_order_end_time = start_time + timedelta(hours=1) # take the photo anytime between start_time, and a day from start_time
+    delivery_deadline = image_order_end_time + timedelta(days=1) # downlink the photo you took by this time
+    image_order = ImageOrder(
+        schedule_id=schedule.id,
+        latitude=0.0,
+        longitude=0.0,
+        image_type='medium',
+        start_time=start_time,
+        end_time=image_order_end_time,
+        delivery_deadline=delivery_deadline,
+        visits_remaining=15,
+        revisit_frequency=timedelta(days=1)
+    )
+    session.add(image_order)
+    session.commit() # populate default fields for image_order
+    schedule_image_order(image_order, schedule, satellites, ground_stations)
+
+
+def schedule_image_order(order: ImageOrder, schedule: Schedule, satellites: list[Satellite], ground_stations: list[GroundStation]):
+    requests = []
+
+    session = get_db_session()
+    # create repeated requests
+    for visit_count in range(order.visits_remaining):
+        requests.append(
+            ScheduleRequest(
+                schedule_id=order.schedule_id,
+                order_id=order.id,
+                order_type=order.order_type,
+                window_start=order.start_time,
+                window_end=order.end_time,
+                duration=order.duration,
+                uplink_size=order.uplink_size,
+                downlink_size=order.downlink_size,
+                power_usage=order.power_usage,
+                delivery_deadline=order.delivery_deadline,
+                priority=order.priority
+            )
+        )
+        order.visits_remaining -= 1
+        order.start_time += order.revisit_frequency
+        order.end_time += order.revisit_frequency
+        order.delivery_deadline += order.revisit_frequency
+        session.commit()
+    
+    session.add_all(requests)
+    session.commit()
+
+
+    contact_duration = timedelta(minutes=10)
+
+    sat = random.choice(satellites)
+    gs = random.choice(ground_stations)
+
+    first_contact = ContactEvent(
+        schedule_id=schedule.id,
+        asset_id=sat.id,
+        groundstation_id=gs.id,
+        start_time=requests[0].window_start - contact_duration,
+        duration=timedelta(minutes=20)
+    )
+    session.add(first_contact)
+    session.commit()
+
+    # partition requests into three separate arrays, evenly based on index
+    num_of_partitions = 7
+    uplink_contact = first_contact
+
+    partition_count = 0
+    while len(requests)>0:
+        partition_count += 1
+
+        scheduled_imaging_partition = []
+        for count in range(ceil(len(requests)/num_of_partitions)):
+            try:
+                request = requests.pop(0)
+            except IndexError:
+                continue
+            if not request:
+                break
+
+            event_start_time = (request.window_end - request.duration)
+            scheduled_imaging_partition.append(
+                ScheduledImaging(
+                    schedule_id=request.schedule_id,
+                    request_id=request.id,
+                    asset_id=sat.id,
+                    start_time=event_start_time,
+                    duration=request.duration,
+                    window_start=request.window_start,
+                    window_end=request.window_end,
+                    uplink_contact_id=uplink_contact.id,
+                    uplink_size=request.uplink_size,
+                    downlink_contact_id=None, # we are soon going to create this
+                    downlink_size=request.downlink_size,
+                    power_usage=request.power_usage,
+                    priority=request.priority
+                )
+            )
+
+        # create a downlink contact and add it as downlink_contact_id for every request in the partition
+        downlink_contact = ContactEvent(
+            schedule_id=schedule.id,
+            asset_id=sat.id,
+            groundstation_id=gs.id,
+            start_time=scheduled_imaging_partition[-1].window_end,
+            duration=timedelta(minutes=20)
+        )
+        session.add(downlink_contact)
+
+        new_sat = random.choice(satellites)
+        new_gs = random.choice(ground_stations)
+
+        if new_sat.id != sat.id or new_gs.id != gs.id:
+            sat = new_sat
+            gs = new_gs
+
+            uplink_contact = ContactEvent(
+                schedule_id=schedule.id,
+                asset_id=sat.id,
+                groundstation_id=gs.id,
+                start_time=downlink_contact.start_time - contact_duration,
+                duration=timedelta(minutes=20)
+            )
+            session.add(uplink_contact)
+        else:
+            uplink_contact = downlink_contact
+
+
+        session.commit()
+        for scheduled_imaging in scheduled_imaging_partition:
+            scheduled_imaging.downlink_contact_id = downlink_contact.id
+        session.add_all(scheduled_imaging_partition)
+        session.commit()
+
+        previous_contact = downlink_contact
+    
+    session.commit()
